bc193b403d4b29602de3071a3df656daf06be445
EnzoFicoseco <enzoficoseco2704@gmail.com>
2024-07-23 10:21:18 -0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Enzo Ficoseco on 23/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
